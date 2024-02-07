131ee0a2347cd6fc875cde0877e180a1edae0677
njwright92 <njwright92@gmail.com>
2024-02-07 13:22:30 -0800

    "&#39;"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
             </h3>
             <ul className="list-disc list-inside space-y-2 mb-4">
               <li>
-                Whether you're a budding comedian, a poet, or a musician,
+                Whether you&#39;re a budding comedian, a poet, or a musician,
                 OpenMicFinder is your gateway to visibility. Our platform
                 connects performers with venues hosting open mic events.
                 Showcase your talent, grow your fan base, and network with other
@@ -125,9 +125,9 @@ export default function Home() {
             </p>
             <p className="mb-4">
               Engage in witty and creative dialogues with ComicBot to develop
-              unique and hilarious comedy routines. Whether you're preparing for
-              a stand-up show or just seeking to enhance your comedic skills,
-              ComicBot is your ideal partner.
+              unique and hilarious comedy routines. Whether you&#39;re preparing
+              for a stand-up show or just seeking to enhance your comedic
+              skills, ComicBot is your ideal partner.
             </p>
             <div className="flex justify-center">
               <Link href="/ComicBot">
