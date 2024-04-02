145e1994afc49ab5a3e3303ed2d97456c1e1cc27
njwright92 <njwright92@gmail.com>
2024-04-02 14:02:50 -0700

    "new loading spinner"

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div role="status" aria-label="loading">
       <svg
-        className="w-6 h-6 stroke-orange-500 animate-spin "
+        className="w-8 h-8 stroke-orange-500 animate-spin "
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -11,14 +11,14 @@ export default function Loading() {
           <path
             d="M14.6437 2.05426C11.9803 1.2966 9.01686 1.64245 6.50315 3.25548C1.85499 6.23817 0.504864 12.4242 3.48756 17.0724C6.47025 21.7205 12.6563 23.0706 17.3044 20.088C20.4971 18.0393 22.1338 14.4793 21.8792 10.9444"
             stroke="stroke-current"
-            stroke-width="1.4"
-            stroke-linecap="round"
+            strokeWidth="1.4"
+            strokeLinecap="round"
             className="my-path"
           ></path>
         </g>
         <defs>
           <clipPath id="clip0_9023_61563">
-            <rect width="24" height="24" fill="zinc-200"></rect>
+            <rect width="32" height="32" fill="zinc-900"></rect>
           </clipPath>
         </defs>
       </svg>
