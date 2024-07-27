bc525b7ab598be2e4f07e7d298c989f199592b67
njwright92 <njwright92@gmail.com>
2024-07-27 15:15:59 -0700

    "update"

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -11,7 +11,7 @@ export default function Loading() {
           <path
             d="M14.6437 2.05426C11.9803 1.2966 9.01686 1.64245 6.50315 3.25548C1.85499 6.23817 0.504864 12.4242 3.48756 17.0724C6.47025 21.7205 12.6563 23.0706 17.3044 20.088C20.4971 18.0393 22.1338 14.4793 21.8792 10.9444"
             stroke="stroke-current"
-            strokeWidth="1.4"
+            strokeWidth="1.5"
             strokeLinecap="round"
             className="my-path"
           ></path>
