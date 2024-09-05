0395c49f177e7bb5d99918e0fccc57d325b9bcd2
njwright92 <njwright92@gmail.com>
2024-09-05 12:28:23 -0700

    "metadata export layout.tsx"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,43 @@
+import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { CityProvider } from "./components/cityContext";
 import { EventProvider } from "./components/eventContext";
 import { HeadlineProvider } from "./components/headlinecontext";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Humor Hub - The Go-To Platform for Everything Comedy",
+  description:
+    "Discover the ultimate destination for everything comedy. Explore jokes, puns, open mic events, and more at Humor Hub.",
+  keywords:
+    "comedy platform, stand-up comedy, funny jokes, puns, open mic events, humor content, AI comedy, comedy writing tools, best jokes, comedy venues",
+  openGraph: {
+    title: "Humor Hub - The Go-To Platform for Everything Comedy",
+    description:
+      "Discover the ultimate destination for everything comedy. Explore jokes, puns, open mic events, and more at Humor Hub.",
+    url: "https://www.thehumorhub.com/",
+    type: "website",
+    images: [
+      {
+        url: "https://www.thehumorhub.com/images/og-image-home.jpg",
+        alt: "Humor Hub Open Graph Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@natebug321",
+    title: "Humor Hub - The Go-To Platform for Everything Comedy",
+    description:
+      "Discover the ultimate destination for everything comedy. Explore jokes, puns, open mic events, and more at Humor Hub.",
+    images: [
+      {
+        url: "https://www.thehumorhub.com/images/og-image-home.jpg",
+        alt: "Humor Hub Twitter Image",
+      },
+    ],
+  },
+};
 
 interface RootLayoutProps {
   children: ReactNode;
@@ -14,6 +49,8 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Favicon */}
         <link
           rel="icon"
           href="/icon.png"
@@ -21,13 +58,23 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
           sizes="any"
           className="rounded-full"
         />
-        <link rel="canonical" href="https://www.thehumorhub.com/" />
 
-        {/* Default Open Graph / Social Sharing Metadata */}
+        {/* Open Graph and Twitter Metadata */}
         <meta property="og:site_name" content="Humor Hub" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@natebug321" />
         <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Humor Hub - The Go-To Platform for Everything Comedy"
+        />
+        <meta
+          property="og:description"
+          content="Discover the ultimate destination for everything comedy. Explore jokes, puns, open mic events, and more at Humor Hub."
+        />
+        <meta property="og:url" content="https://www.thehumorhub.com/" />
+        <meta
+          property="og:image"
+          content="https://www.thehumorhub.com/images/og-image-home.jpg"
+        />
       </head>
       <body>
         <CityProvider>
