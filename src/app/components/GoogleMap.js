ac59b93dc9b2d557402683f0b680b59e06e846e4
njwright92 <njwright92@gmail.com>
2024-09-10 08:26:38 -0700

    "border-Radius"

--- src/app/components/GoogleMap.js
+++ src/app/components/GoogleMap.js
@@ -8,7 +8,7 @@ let API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 const loader = new Loader({
   apiKey: API_KEY,
   version: "weekly",
-  libraries: ["marker"], // Add the 'marker' library for advanced markers
+  libraries: ["marker"],
 });
 
 const GoogleMap = ({ lat, lng, events }) => {
@@ -16,23 +16,21 @@ const GoogleMap = ({ lat, lng, events }) => {
 
   const loadMap = useCallback(async () => {
     try {
-      await loader.load(); // Load the Google Maps JavaScript API
+      await loader.load();
 
-      const { google } = window; // Access the google object from the global window object
+      const { google } = window;
       const { AdvancedMarkerElement } = await google.maps.importLibrary(
         "marker"
-      ); // Import AdvancedMarkerElement
+      );
 
       if (mapContainerRef.current) {
         const map = new google.maps.Map(mapContainerRef.current, {
           center: { lat, lng },
           zoom: 10,
-          mapId: "DEMO_MAP_ID", // Add a map ID (required for advanced markers)
+          mapId: "DEMO_MAP_ID",
         });
 
-        // Loop through the events to create markers
         events.forEach((event, i) => {
-          // Validate lat and lng before creating a marker
           const eventLat = parseFloat(event.lat);
           const eventLng = parseFloat(event.lng);
 
@@ -43,15 +41,14 @@ const GoogleMap = ({ lat, lng, events }) => {
               position,
               map,
               title: `${i + 1}. ${event.name}`,
-              gmpClickable: true, // Enable clicking
+              gmpClickable: true,
             });
 
-            // Create the info window
             const infoWindow = new google.maps.InfoWindow();
 
-            // Add click event listener for the marker
             marker.addListener("click", ({}) => {
-              infoWindow.close(); // Close any open info windows
+              infoWindow.close();
+              s;
               infoWindow.setContent(`
                 <div style="background-color: #f5f5f4; color: #18181b; padding: .75rem; border-radius: 3rem;">
                   <h2 style="margin: .25rem; font-size: 1rem; font-weight: bold;">${event.name}</h2>
@@ -59,7 +56,7 @@ const GoogleMap = ({ lat, lng, events }) => {
                   <p style="margin: 0; font-size: .8rem;">${event.date}</p>
                 </div>
               `);
-              infoWindow.open(marker.map, marker); // Open the info window at the marker's location
+              infoWindow.open(marker.map, marker);
             });
           } else {
             console.warn(`Invalid coordinates for event: ${event.name}`);
@@ -79,7 +76,7 @@ const GoogleMap = ({ lat, lng, events }) => {
     <div
       id="map"
       ref={mapContainerRef}
-      style={{ height: "25rem", width: "100%" }}
+      style={{ height: "25rem", width: "100%", borderRadius: "1rem" }}
     ></div>
   );
 };
