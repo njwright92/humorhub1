b2c8d7db98b5c0395d88b7e7ae449a9e3db4734c
njwright92 <njwright92@gmail.com>
2024-10-27 14:10:22 -0700

    "mobile"

--- src/app/MicFinder/page.tsx
+++ src/app/MicFinder/page.tsx
@@ -623,33 +623,30 @@ const EventsPage = () => {
       <Header />
       <div className="screen-container">
         <OpenMicBanner />
-        <h1 className="title text-4xl sm:text-2xl xs:text-xl font-bold text-center mb-6">
+        <h1 className="title text-4xl sm:text-xl xs:text-lg font-bold text-center mb-6">
           Discover Open Mic Events!
         </h1>
 
-        <p className="text-lg md:text-xl sm:text-base xs:text-sm text-center mt-4 mb-6">
+        <p className="text-lg md:text-lg sm:text-md xs:text-sm text-center mt-4 mb-6">
           Know or host an amazing open mic event? Share it with the local
-          community through MicFinder. Whether it&apos;s
-          comedy, music, poetry, or any live performance, let&apos;s spread the
-          word and fill the room!
+          community through MicFinder. Whether it&apos;s comedy, music, poetry,
+          or any live performance, let&apos;s spread the word and fill the room!
         </p>
 
         <div className="text-center mb-8">
           <MemoizedEventForm />
         </div>
 
-        <h2 className="text-lg font-semibold text-center mt-4 sm:mt-2 mb-4 xs:mb-2">
+        <h2 className="text-md font-semibold text-center mt-4 sm:mt-2 mb-4 xs:mb-2">
           Ready to explore? Select your city and date to find local events!
         </h2>
 
-        <p className="text-md text-center mb-4 sm:mb-2 xs:mb-1">
+        <p className="text-sm text-center mb-4 sm:mb-2 xs:mb-1">
           Scroll to see all upcoming events and discover your next stage.
         </p>
 
         <div className="flex flex-col justify-center items-center mt-2">
-          {/* Dropdown with search input */}
           <div className="relative w-full max-w-xs">
-            {/* Button to open/close dropdown */}
             <div
               className="modern-input cursor-pointer bg-zinc-100 text-zinc-900"
               onClick={() => {
