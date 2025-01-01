3085854e5d38f9fa045452a79da55230a0cdd41b
njwright92 <njwright92@gmail.com>
2024-12-31 16:46:32 -0800

    "npm update and banner update"

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -301,7 +301,7 @@ export default function Header() {
       text-yellow-400 text-md bg-zinc-700 rounded-full p-1 shadow-xl animate-bounce
       md:fixed md:top-5 md:right-5 md:left-auto"
               >
-                *Now adding Comedy Festivals/competitions!*
+                *1700+ events and growing!*
               </div>
             )}
 
