d3c2fe7585de5bc3745c0ac526d6a54e64d68403
njwright92 <njwright92@gmail.com>
2025-02-21 07:41:10 -0800

    "npm update and banner update"

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -302,7 +302,7 @@ export default function Header() {
       text-yellow-400 text-md bg-zinc-700 rounded-full p-1 shadow-xl animate-bounce
       md:fixed md:top-5 md:right-5 md:left-auto"
               >
-                *The event list has been cleaned, more up to date than ever!*
+                *The event list is more up to date than ever!*
                 <br />
                 *Mobile App coming soon!*
               </div>
