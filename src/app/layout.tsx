adfec49139ab84f945670f868bcf6d81399559bd
njwright92 <njwright92@gmail.com>
2025-12-05 09:14:18 -0800

    "lazy on load and delay for gtm in layout"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,15 +88,23 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       >
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-KVJSFKV8');
-            `,
+      function loadGTM() {
+        if (window.gtmLoaded) return;
+        window.gtmLoaded = true;
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KVJSFKV8');
+      }
+      setTimeout(loadGTM, 3500);
+      ['scroll','click','touchstart','keydown'].forEach(e => 
+        window.addEventListener(e, loadGTM, {once: true, passive: true})
+      );
+    `,
           }}
         />
         <ToastProvider>{children}</ToastProvider>
