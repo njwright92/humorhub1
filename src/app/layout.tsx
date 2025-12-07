9aa900a082debd423f0a50ee178c1da1f40bd416
njwright92 <njwright92@gmail.com>
2025-12-07 10:26:27 -0800

    "tag manager"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
+import Script from "next/script";
 import { Comic_Neue } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from "./components/ToastContext";
@@ -80,6 +81,17 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body
         className={`${comicNeue.variable} bg-zinc-900 text-zinc-200 antialiased`}
       >
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KVJSFKV8');`,
+          }}
+        />
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-KVJSFKV8"
