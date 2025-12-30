d28958dbfba5945b686bab0958091fd3ab1a658b
njwright92 <njwright92@gmail.com>
2025-12-29 18:04:36 -0800

    "pre-connects to profile"

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -29,14 +29,18 @@ export const metadata: Metadata = {
 
 export default function ProfilePage() {
   return (
-    <main className="grid min-h-screen content-start gap-4 p-2 pt-12 text-center md:ml-20">
-      <h1 className="font-heading text-3xl font-bold tracking-wide text-amber-700 text-shadow-md sm:text-4xl">
-        Profile Management
-      </h1>
-      <p className="text-sm text-stone-300 md:text-lg">
-        Manage your personal schedule
-      </p>
-      <ProfileClient />
-    </main>
+    <>
+      <link rel="preconnect" href="https://identitytoolkit.googleapis.com" />
+      <link rel="preconnect" href="https://humorhub-73ff9.firebaseapp.com" />
+      <main className="grid min-h-screen content-start gap-4 p-2 pt-12 text-center md:ml-20">
+        <h1 className="font-heading text-3xl font-bold tracking-wide text-amber-700 text-shadow-md sm:text-4xl">
+          Profile Management
+        </h1>
+        <p className="text-sm text-stone-300 md:text-lg">
+          Manage your personal schedule
+        </p>
+        <ProfileClient />
+      </main>
+    </>
   );
 }
