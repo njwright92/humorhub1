ed0c969f9656ae8fb28bdb028995e136dc4e2318
njwright92 <njwright92@gmail.com>
2026-05-01 23:42:45 -0700

    "fix profile image storage bucket"

--- src/app/lib/firebase-admin.ts
+++ src/app/lib/firebase-admin.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, cert, type App } from "firebase-admin/app";
 import { getAuth as getAdminAuth, type Auth } from "firebase-admin/auth";
 import {
-  initializeFirestore, // <- CHANGED from getFirestore as getAdminFirestore
+  initializeFirestore,
   type Firestore,
 } from "firebase-admin/firestore";
 import {
